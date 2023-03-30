c7b25388d90ec32b961895fcb6ce214ccd8f619d
dungnguyen2110 <dungnguyen912002@gmail.com>
2023-03-30 15:58:43 +0700

    Payment Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import ScheduleSelection from './pages/ScheduleSelection';
 import SeatSelection from './pages/SeatSelection';
 import Payment from './pages/Payment';
+import PaymentSuccess from './pages/PaymentSuccess';
 import './App.css';
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
           <Route path="/schedule" element={<ScheduleSelection/>} />
           <Route path="/seats" element={<SeatSelection/>} />
           <Route path="/payment" element={<Payment/>} />
+          <Route path="/payment/success" element={<PaymentSuccess/>} />
         </Routes>
       </Router>
     </div>
@@ -24,3 +27,6 @@ function App() {
 }
 
 export default App;
+
+
+
---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+const images = {
+    paymentsuccess: require("./paymentsuccess.png"),
+    momoLogo: require("./momoLogo.png"),
+    zaloLogo: require("./zaloLogo.png"),
+    ocbLogo: require("./ocbLogo.png"),
+    vcbLogo: require("./vcbLogo.png"),
+    
+    
+  };
+  
+  export default images;
+  
